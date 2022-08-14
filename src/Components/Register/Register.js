9661e689ffe644d38eb489c6d44ed04f5c4dcfaf
GayanaJinde <jindegayana@gmail.com>
2022-08-14 19:33:56 +0200

    connecting to backend

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('http://https://morning-spire-82470.herokuapp.com//register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
